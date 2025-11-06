b2d707bcbab650bf196a9ad28b878f4cb3a927fd
ukiran03 <ushakiranreddi@gmail.com>
2025-11-06 16:20:32 +0530

    new: ~add~ flag can read from STDIN for new tasks
    remove ~task~ flag in favour of ~add~
    added ~del~ flag to delete tasks from file

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 	"ukiran/todo"
 )
 
@@ -15,13 +18,15 @@ func main() {
 		flag.PrintDefaults()
 	}
 	file := flag.String("file", "", "Write the todo items to File")
-	task := flag.String("task", "", "Task to be included in the todo list\nA quoted string")
+	add := flag.Bool("add", false, "Add task to the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
+	delete := flag.Int("delete", 0, "Item to be deleted")
 	flag.Parse()
 
 	if *file != "" {
 		todoFileName = *file
+		fmt.Println(todoFileName)
 	}
 
 	l := &todo.List{}
@@ -34,7 +39,15 @@ func main() {
 	switch {
 	case len(os.Args) == 1 || *list:
 		fmt.Print(l)
-
+	case *delete > 0:
+		if err := l.Delete(*delete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	case *complete > 0:
 		if err := l.Complete(*complete); err != nil {
 			fmt.Fprintln(os.Stderr, err)
@@ -44,8 +57,13 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-	case *task != "":
-		l.Add(*task)
+	case *add:
+		task, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(task)
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
@@ -54,4 +72,21 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Invalid option")
 		os.Exit(1)
 	}
+}
+
+// getTask function decides where to get the description for a new
+// task from: arguments or STDIN
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("Task cannot be blank")
+	}
+	return s.Text(), nil
 }
--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -41,20 +42,35 @@ func TestTodoCLI(t *testing.T) {
 	cmdPath := filepath.Join(dir, binName)
 
 	t.Run("AddNewTask", func(t *testing.T) {
-		cmd := exec.Command(cmdPath, "-task", task)
+		cmd := exec.Command(cmdPath, "-add", task)
 		if err := cmd.Run(); err != nil {
 			t.Fatal(err)
 		}
 	})
+
+	task2 := "test task number 2"
+	t.Run("AddNewTaskFromSTDIN", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-add")
+		cmdStdin, err := cmd.StdinPipe()
+		if err != nil {
+			t.Fatal(err)
+		}
+		io.WriteString(cmdStdin, task2)
+		cmdStdin.Close()
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
 	t.Run("ListTasks", func(t *testing.T) {
 		cmd := exec.Command(cmdPath, "-list")
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			t.Fatal(err)
 		}
-		expected := fmt.Sprintf("  1: %s\n", task)
+		expected := fmt.Sprintf("  1: %s\n  2: %s\n", task, task2)
 		if expected != string(out) {
-			t.Errorf("Expected %q, got %q instead\n", expected, string(out))
+			t.Errorf("\nExpected %q\nGot %q\n", expected, string(out))
 		}
 	})
 }
